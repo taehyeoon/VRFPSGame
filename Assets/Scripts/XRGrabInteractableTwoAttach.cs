3fe601613fbefe164524128060d3ce914b8676c4
zzinnu <jjinwoo1004@gmail.com>
2023-04-28 17:31:24 +0900

    권총 판정 업글 및 탄창 haptic 추가
    1. XRGrabInteractableTwoAttach.cs의 OnSelectEntered 수정, OnSelectExited 추가.
    colliders[0]은 판정을 위한 collider로 간주
    
    2. 탄창에 haptic interactable 추가

--- Assets/Scripts/XRGrabInteractableTwoAttach.cs
+++ Assets/Scripts/XRGrabInteractableTwoAttach.cs
@@ -8,6 +8,12 @@ public class XRGrabInteractableTwoAttach : XRGrabInteractable
     public Transform leftAttachTransform;
     public Transform rightAttachTransform;
 
+    /*
+     * OnSelectEneterd
+     * 1. 왼손, 오른손 구분
+     * 2. 오브젝트 전체를 감싸는 collider 제거
+     *      colliders[0]은 grab 판정 전용임. grab 이후엔 없어야 다른 손으로 interaction할 수 있음
+     */
     protected override void OnSelectEntered(SelectEnterEventArgs args)
     {
         if (args.interactorObject.transform.CompareTag("Left"))
@@ -19,6 +25,13 @@ public class XRGrabInteractableTwoAttach : XRGrabInteractable
             attachTransform = rightAttachTransform;
         }
 
+        args.interactableObject.colliders[0].enabled = false;
         base.OnSelectEntered(args);
+    }
+
+    protected override void OnSelectExited(SelectExitEventArgs args)
+    {
+        args.interactableObject.colliders[0].enabled = true;
+        base.OnSelectExited(args);
     }
 }
